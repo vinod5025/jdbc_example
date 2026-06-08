149c10cc1a915293b96505d4d3eec620aec31f4c
yemulyuvraj <163127578+yemulyuvraj@users.noreply.github.com>
2026-06-08 14:28:38 +0530

    add

--- src/main/java/org/tehhub/com/Demo.java
+++ src/main/java/org/tehhub/com/Demo.java
@@ -3,10 +3,8 @@ package org.tehhub.com;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+System.out.println("I love you");		// TODO Auto-generated method stub
 
-		System.out.println("okkkk");
-		System.out.println("Atul Bhaii...!"); 
-		System.out.println("Vinod Thombare....!");
-	}  
+	}
+
 }
